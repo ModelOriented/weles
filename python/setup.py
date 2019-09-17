855fa3f5e1bb640918eb8575f4a6e558645ac1d4
Wojciech Kretowicz <32495076+WojciechKretowicz@users.noreply.github.com>
2019-09-17 14:09:54 +0200

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='weles',
 	version='1.0',
-	description='The Python client package for communication with model governance base "vimo".',
+	description='The Python client package for communication with model governance base "weles".',
 	url='https://github.com/WojciechKretowicz/vimo',
 	author='Wojciech Kretowicz',
 	author_email='wojtekkretowicz@gmail.com',
